74298f4a4bf894e9f1f8fd603fb5bff7d9f164c1
John <spriggsjohnw@gmail.com>
2020-11-04 16:50:18 -0800

    Updated the project part of the portfolio

--- public/scripts/master.js
+++ public/scripts/master.js
@@ -93,7 +93,7 @@ app.bio = () => {
    const birthday = () => {const date = new Date;console.log(date);const rawAge = date.getFullYear() - 1981;const check = date.getMonth() + 1 > 9 || date.getMonth() + 1 == 9 && date.getDate() >= 29 ? 0 : 1;return String(rawAge - check);};
    const stats = {name:'John Spriggs',age:birthday(),country:'United States',state:'California',cats_or_dogs:'dogs',employment_status:'unemployed - actively looking'};
    const p1 = "My name is John Spriggs, and I used to be a Delivery Support Analyst for VizExplorer.  At VizExplorer, I communicated with clients holding high-level rankings within their organization about issues or questions with our software.  As an after-hours scheduled employee, I dealt with complex problems, mainly alone with no supervision, that required SQL, JavaScript, NodeJS, and systems OS knowledge to process.  After reviewing, reproducing, and researching these problems, I would correct the issue.  In instants of extensive changes to the application/software or the scope is outside of the teams' area of expertise. I would provide detailed information from my research, the place I believe the problem(s)' located, suggestions on how I think the issue should be resolved, and escalate to the correct department to review."
-   const p2 = "I have always enjoyed learning new and challenging skills, especially skill technically related.  Learning Nodejs, JavaScript, and HTML5/CSS3 (<span class='credit-bio'>https://accredible.com/16045116</span>) has been challenging, but creating new apps or figuring out errors or problems without using google searches has been fun and inspiring.  If I'm not in front of a computer, you can find me with my wonderful family, spending time with my 2-year-old son, or laughing with my beautiful spouse.  I believe that you should treat people as you would like to be treated, which goes a long way once applied to the customer service you provide."
+   const p2 = "I have always enjoyed learning new and challenging skills, especially skill technically related.  Learning Nodejs, JavaScript, and HTML5/CSS3 (<span class='url'>https://accredible.com/16045116</span>) has been challenging, but creating new apps or figuring out errors or problems without using google searches has been fun and inspiring.  If I'm not in front of a computer, you can find me with my wonderful family, spending time with my 2-year-old son, or laughing with my beautiful spouse.  I believe that you should treat people as you would like to be treated, which goes a long way once applied to the customer service you provide."
    const header = [{main:'header',type:'div',id:'header-bio'},{main:'header>div',type:'img',cn:'my-bio-pic',src:'./public/images/myimage.jpg'},{main:'header>div',type:'div',id:'bio-stats'}];
    const main = [{main:'main',type:'div',cn:'info-bio'},{main:'main>div',type:'p',text:p1},{main:'main>div',type:'p',html:p2}];
    const displayStats = [];
@@ -108,11 +108,11 @@ app.bio = () => {
    }
    app.loadUp(displayStats);
    app.loadUp(main);
-   app.credit();
+   app.urlLinks();
 };
 
 app.projects = () => {
-   const projects = [{title:'PM2-Watcher',app_type:'NPM Node Module',app_tech:'NodeJS, ExpressJS, Pm2, JavaScript, HTML5 & CSS3',about_app:"I created pm2-watcher for apps/processes that depend on pm2.  My module will allow a user or even a non-technical user to monitor pm2 without watching the server.  Unlike some created apps that update you when the app(s)/process(es) go down.  The module I created monitors all pm2 processes; if a process stops, fails, or errors, the app will send out an email notification.  If that same process was to come back online, the app sends out a follow-up email.\n\nThe app allows delays between the notification and the failed/recovered process, and you have an option to include your process' logs for failure emails.  The app keeps a record of your email messages, the email message status, and any failed/recovered process.",app:'https://www.npmjs.com/package/pm2-watcher',github:'https://github.com/spriggs81/pm2-watcher'},{title:'Support Tool',app_type:'web application',about_app:"I created this app after completing a online web developer bootcamp "}];
+   const projects = [{title:'PM2-Watcher',app_type:'NPM Node Module',app_tech:'NodeJS, ExpressJS, Pm2, JavaScript, HTML5 & CSS3',problem:"Pm2 didn't send out automatic notifications unless part of a plan that could get costly. If someone wasn't watching the server, they could miss issues that could affect the application's stability.  In some cases, the client may not notice a minor problem that could lead to a critical one.",solution:"This application monitors all pm2 processes and will send out an email notification if a process goes offline.  If a process recovers, then a follow-up email is sent out.",app:'https://www.npmjs.com/package/pm2-watcher',github:'https://github.com/spriggs81/pm2-watcher'},{title:'Error Logs',app_type:'Web Application',app_tech:'NodeJS, ExpressJS, EJS, Event-Stream, SQL-Formatter, Bootstrap 4.3.1',problem:"When researching a client related issue, the log files could explain 85% of the client's problem. Due to the problematic display, it could be easy for someone to overlook essential data related to the client's situation.",solution:"Creating the app was meant to make the logs more readable and less intimidating.  Allowing the support team to be able to resolve issues in a more timely manner.",app:'https://error-logs.herokuapp.com/',github:'https://github.com/spriggs81/error_logs'},{title:'Support Tool',app_type:'web application',app_tech:'NodeJS, ExpressJS, PassportJS, MongoDB, MongooseJS, EJS, and Semantic UI',problem:"In my department(Support), we depended on information from a site controlled by another department.  Sometimes this information was out of date, incorrect, misleading, or missing necessary steps.  It reached the point that team members were sharing notes that had on clients.",solution:"Creating the app was meant to be a one-stop site to pick up information for our team.  Allowing the content to be updated without delays and a place to keep important information.  (Please note, I updated the  app to be more general as it was made for specific products originally.)",app:'http://supporttools.ga',github:'https://github.com/spriggs81/portfolio'}];
    const header = [];
    const main = [{main:'main',type:'div',id:'info-projects'}];
    app.removeAll('header');
@@ -121,16 +121,34 @@ app.projects = () => {
    let count = 0;
    for(project of projects){
       count++
-      const itemSpace = {main:'#info-projects',cn:'grid-1',type:'div',id:'project'+count};
+      const itemSpace = {main:'#info-projects',cn:'project-place',type:'div',id:'project'+count};
       const itemTitle = {main:'#project'+count,cn:'grid-1',type:'h1',text:project.title};
-      const itemStatHolder = {main:'#project'+count,id:'statP'+count,cn:'stats',type:'section'}
-      const itemStat1 = {main:'#statP'+count,type:'span',id:'p'+count+'-stat1',cn:'stat-items',text:'Project Type: '+project.app_type};
-      const itemStat2 = {main:'#statP'+count,type:'span',id:'p'+count+'-stat2',cn:'stat-items',text:'Project Tech: '+project.app_tech};
-      const itemBody = {main:'#project'+count,cn:'grid-1',type:'h3',text:project.about_app};
-      items.push(itemSpace,itemTitle,itemStatHolder,itemStat1,itemStat2,itemBody);
+      const itemStatHolder = {main:'#project'+count,id:'statP'+count,cn:'stats',type:'section'};
+      const itemStat1 = {main:'#statP'+count,type:'p',id:'p'+count+'-stat1',cn:'stat-items',html:'<span class="lab">Project Type</span>: '+project.app_type};
+      const itemStat2 = {main:'#statP'+count,type:'p',id:'p'+count+'-stat2',cn:'stat-items',html:'<span class="lab">Project Tech</span>: '+project.app_tech};
+      const itemProblem = {main:'#project'+count,cn:'grid-1',type:'p',html:'<span class="lab">The Problem:</span>\n'+project.problem};
+      const itemSolution = {main:'#project'+count,cn:'grid-1',type:'p',html:'<span class="lab">The Solution:</span>\n'+project.solution};
+      items.push(itemSpace,itemTitle,itemStatHolder,itemStat1,itemStat2,itemProblem,itemSolution);
+      const itemUrlHolder = {main:'#project'+count,cn:'url-place',type:'div',id:'url-holder'+count};
+      if(project.app || project.github){
+         items.push(itemUrlHolder);
+         if(project.app){
+            const theApp = {main:'#url-holder'+count,type:'p',id:'app-url-p'+count+'-stat1',cn:'url-item',html:'App\'s URL: <span class="url">'+project.app+'</span>'};
+            items.push(theApp);
+         }
+         if(project.github){
+            const githubUrl = {main:'#url-holder'+count,type:'p',id:'app-url-p'+count+'-stat2',cn:'url-item',html:'GitHub URL: <span class="url">'+project.github+'</span>'};
+            items.push(githubUrl);
+         }
+         if(count > 1){
+           const hrPlacer = {main:'#project'+(count-1),type:'hr'};
+           items.push(hrPlacer);
+         }
+      }
    }
    app.loadUp(main);
    app.loadUp(items);
+   app.urlLinks();
 }
 
 app.arrowShow = () => {
@@ -174,8 +192,6 @@ app.loadNewStatement = (num) => {
       const placement = document.querySelector('.words');
       app.arrowShow();
       placement.innerText = app.statements[num];
-   } else {
-      clearInterval(interval);
    }
 }
 
@@ -194,12 +210,14 @@ app.mobileMenu = () => {
    }
 }
 
-app.credit = () => {
-   if(document.querySelector('.credit-bio')){
-      const creditBio = document.querySelector('.credit-bio');
-      creditBio.addEventListener('click',() => {
-         window.open('https://accredible.com/16045116');
-      });
+app.urlLinks = () => {
+   if(document.querySelector('.url')){
+      const urls = document.querySelectorAll('.url');
+      for(link of urls){
+         link.addEventListener('click',(e) => {
+            window.open(e.target.innerText);
+         });
+      }
    }
 }
 
@@ -211,8 +229,8 @@ app.loadUp = (data) => {
 
 app.init = () => {
    app.menuButtons();
-   app.home();
-   // app.projects();
+   // app.home();
+   app.projects();
 }
 
 app.notSetup = () => {
